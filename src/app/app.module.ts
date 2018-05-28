d6456e535e4721f25d2008c70db4aacf4bbc864f
Nereo Lopez <nereolopez@imac-de-nereo.home>
2018-05-28 19:06:51 +0200

    Home Component Text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule, MatTableModule, MatCheckboxModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule, MatTableModule, MatCheckboxModule, MatCardModule } from '@angular/material';
 import { ContextualToolbarModule } from 'nereo-material';
 
 import { AppComponent } from './app.component';
 import { ContextualToolbarDemoComponent } from './contextual-toolbar-demo/contextual-toolbar-demo.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContextualToolbarDemoComponent
+    ContextualToolbarDemoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +27,22 @@ import { ContextualToolbarDemoComponent } from './contextual-toolbar-demo/contex
     MatTooltipModule,
     MatTableModule,
     MatCheckboxModule,
+    MatCardModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'contextual-toolbar',
+        component: ContextualToolbarDemoComponent
+      },
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
